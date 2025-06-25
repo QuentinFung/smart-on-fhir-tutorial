de6be0fc25b9cd5aee976e3625bec99f13db31c3
Quentin Fung <quentin.fung@oracle.com>
2025-06-25 15:52:37 +0100

    remove observations

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -11,6 +11,7 @@
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
+        /*
         var obv = smart.patient.api.fetchAll({
                     type: 'Observation',
                     query: {
@@ -21,11 +22,12 @@
                       }
                     }
                   });
+         */
 
-        $.when(pt, obv).fail(onError);
+        $.when(pt).fail(onError);
 
-        $.when(pt, obv).done(function(patient, obv) {
-          var byCodes = smart.byCodes(obv, 'code');
+        $.when(pt).done(function(patient, obv) {
+          //var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
 
           console.log(patient);
@@ -62,11 +64,13 @@
             lname = patient.name[0].family.join(' ');
           }
 
+          /*
           var height = byCodes('8302-2');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
+          */
 
           var p = defaultPatient();
           p.id = patient.id;
@@ -74,6 +78,8 @@
           p.gender = gender;
           p.fname = fname;
           p.lname = lname;
+
+          /*
           p.height = getQuantityValueAndUnit(height[0]);
 
           if (typeof systolicbp != 'undefined')  {
@@ -86,6 +92,7 @@
 
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
+          */
 
           ret.resolve(p);
         });
@@ -150,11 +157,13 @@
     $('#lname').html(p.lname);
     $('#gender').html(p.gender);
     $('#birthdate').html(p.birthdate);
+    /*
     $('#height').html(p.height);
     $('#systolicbp').html(p.systolicbp);
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
+    */
   };
 
 })(window);
